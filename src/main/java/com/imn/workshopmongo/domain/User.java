09f5789d045d4825ee5cc7e42ec482601919242b
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-05-19 06:36:21 -0300

    Entity User and basic REST

---
+++ src/main/java/com/imn/workshopmongo/domain/User.java
@@ -0,0 +1,83 @@
+package com.imn.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public User() {}
+		
+	public User(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
