e063f9d3a4e75a5267d5f13c6a807d6c0084e9a7
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-05-24 06:53:01 -0300

    Query complex criteria

--- src/main/java/com/imn/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/imn/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.imn.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,5 +15,15 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
 	List<Post> searchTitle(String text);
 	
+	
+	//this method will produce the same result as searchTitle, but using JPA to create the 
+	//	query to do the search, the method name needs to start with 
+	//	findBy<followed by the field and criteria>
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {'date':{$gte: ?1} }, {'date':{$lte: ?2} },"
+				+ "{ $or: [ {'title': {$regex: ?0, $options: 'i'},"
+						+ "{'body': {$regex: ?0, $options: 'i' },"
+						+ "{'comments.text': {$regex: ?0, $options: 'i' } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
