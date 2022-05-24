848a1f4a06eef9b5d0f84d4f4896cf53efe760ed
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-05-24 05:42:21 -0300

    Get Post by ID

---
+++ src/main/java/com/imn/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.imn.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.imn.workshopmongo.domain.Post;
+import com.imn.workshopmongo.repositories.PostRepository;
+import com.imn.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo; 
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objetct not found"));
+	}
+	
+}
