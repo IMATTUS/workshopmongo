c60a4582976dc0466aa03a4e8b1f113af37f3c48
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-05-24 06:16:05 -0300

    Simple consult using query methods

--- src/main/java/com/imn/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/imn/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.imn.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.imn.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
